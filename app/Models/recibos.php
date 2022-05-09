4c6c03c5b2d8566d54f534aa2bc8bac44588ee26
Imeguras <imeguras@teknik.io>
2022-05-09 10:57:21 +0100

    created some crappy model's

---
+++ app/Models/recibos.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class recibos extends Model
+{
+    use HasFactory;
+	protected $table = "recibos"; 
+}
