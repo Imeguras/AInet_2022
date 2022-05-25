61225d09a127812a744b2539c89fdf845e2fb248
Kibo <2181160@my.ipleiria.pt>
2022-05-25 12:03:10 +0100

    Added Basic Stuff

--- app/Models/Genero.php
+++ app/Models/Genero.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Filmes
 
 class generos extends Model
 {
     use HasFactory;
 	protected $table = "generos"; 
+
+	public function filmes()
+    {
+        return $this->hasMany(Filmes::class);
+    }
 }
