7e7e46ceb8e7a5a36f6b893936094569656f8be6
Imeguras <imeguras@teknik.io>
2022-06-18 21:09:26 +0100

    Feito

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Models\Users;
+use App\Models\Cliente;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,10 +66,19 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+		$user=User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+			
         ]);
+		Cliente::create([
+			'id'=>$user->id,
+			'nif' => "000000000",
+			'tipo_pagamento' => 'PAYPAL',
+			'ref_pagamento' => "000000000"
+		]);
+
+        return $user;
     }
 }
