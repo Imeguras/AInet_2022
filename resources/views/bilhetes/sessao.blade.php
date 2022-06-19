657a64eadc29179a63bf7b4476cacc4bd5582c57
Imeguras <imeguras@teknik.io>
2022-06-19 23:16:00 +0100

    hydration pause

--- resources/views/bilhetes/sessao.blade.php
+++ resources/views/bilhetes/sessao.blade.php
@@ -14,22 +14,41 @@
 			{{--on submit launch a javascript function--}}
 			
 			<div class="input-group">
-				<input id="idInput" type="text" class="form-control" placeholder="Pesquisar...">
+				<input id="idInput" type="text" class="form-control" placeholder="Validar bilhete nº...">
 				<input id="submitId" type="button" class="btn btn-primary" value="Pesquisar">
 				
 			</div>
-			<div id="resultado">
+			<div class="alert" role="alert">
+				<h4 id="resultado" class="alert-heading"></h4>
+				<p id="bilheteParam"> </p>
+				{{--add a hidden form with a post request to useBilhete--}}
+				
+				{{--<form id="useBilhete" method="post" action="{{ route('useBilhete') }}">
+					@csrf
+					<input id="bilheteId" name="bilheteId" type="hidden">
+					
+				</form>--}}
 			</div>
+				
+			
 		</div>
 	</div>
 </div>
 <script>
-	document.getElementById
+	document.getElementById("submitId").addEventListener("click", function(e){
+		e.preventDefault();
+		showEstado();
+	});
 //read the value of idInput and print the value of the input whose id is the value of idInput
-function showEstado(){
-	var idInput = document.getElementById("idInput").value;
-	var estado = document.getElementById(idInput).value;
-	document.getElementById("resultado").innerHTML = estado;
-}
+	function showEstado(){
+		var idInput = document.getElementById("idInput").value;
+		var estado = document.getElementById(idInput).value;
+		var bilheteparam = document.getElementById("bilheteParam");
+		
+		document.getElementById("resultado").innerHTML = estado;
+		bilheteparam.innerHTML= "O bilhete verificado é o nº: "+idInput; 
+		estado=="não usado"?document.getElementById("resultado").parentElement.className="alert alert-success":document.getElementById("resultado").parentElement.className="alert alert-danger";
+
+	}
 </script>
 @endsection
