cc6a060cd3773dd003db4e9b4f6fa0f3525382d9
Imeguras <imeguras@teknik.io>
2022-06-20 18:37:17 +0100

    Back again

--- resources/views/sessoes/index.blade.php
+++ resources/views/sessoes/index.blade.php
@@ -22,6 +22,11 @@
 						<option value="1">Dia unico</option>
 						<option value="2">Intervalo de dias</option>
 						<option value="3">Todas as...</option>
+					</select>
+					<div id="inject_stuff">
+					</div>
+					<div id="inject_results">
+					</div>
 				</div>
 				<input class="btn btn-primary" type="submit" value="Adicionar Sessoes"></button>
 			</form>
@@ -32,7 +37,14 @@
 //add an change listener to recurrer select
 document.getElementById("recurrer").addEventListener("change", function(){
 	var value = this.value;
+	//remove every child from inject_results
+	var inject_results = document.getElementById("inject_results");
+	while (inject_results.firstChild) {
+		inject_results.removeChild(inject_results.firstChild);
+	}
+	
 	if(value == 1){
+		do
 		//ask for a date and insert that input hidden on the form
 	}else if(value == 2){
 		//ask for two dates and insert the dates on inputs hidden on the form
